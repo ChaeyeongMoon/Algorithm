dfef240c2d8d4ea545013ff7384fc4165dfe1012
ChaeyeongMoon <121281644+ChaeyeongMoon@users.noreply.github.com>
2023-04-03 21:33:00 +0900

    [Bronze I] Title: 평균, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.cc
+++ 백준/Bronze/1546. 평균/평균.cc
@@ -4,9 +4,11 @@ using namespace std;
 int main() {
 	int n;
 	cin >> n;
-	double arr[1000];
-	int max = -1;
+
+	int arr[1000];
+	double max = 0;
 	double sum = 0;
+
 	for (int i = 0; i < n; i++) {
 		cin >> arr[i];
 		if (arr[i] > max) {
@@ -14,9 +16,8 @@ int main() {
 		}
 	}
 	for (int i = 0; i < n; i++) {
-		sum += arr[i]/max*100;
+		sum += arr[i] / max * 100;
 	}
-	cout << sum / n;
+	double res = sum / n;
+	cout << res;
 }
-
-///double형 때매 틀림
