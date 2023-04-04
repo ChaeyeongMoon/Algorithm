1e83b1d2ca7b28e00a8a38bed53c0ad093403bde
ChaeyeongMoon <121281644+ChaeyeongMoon@users.noreply.github.com>
2023-04-04 12:57:33 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 40 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
@@ -1,20 +1,25 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-int N, M, pSum[100001];
+
+
 int main() {
-    ios::sync_with_stdio(false); cout.tie(NULL); cin.tie(NULL);
-    // 입력받으면서 prefix sum을 채움(1칸이 더 필요함에 유의)
-    cin >> N >> M;
-    for (int i = 0; i < N; ++i) {
-        int A;
-        cin >> A;
-        pSum[i + 1] = pSum[i] + A;
-    }
-    // s~e 성분 합 = pSum[e] - pSum[s-1]
-    for (int i = 0; i < M; ++i) {
-        int s, e;
-        cin >> s >> e;
-        cout << pSum[e] - pSum[s - 1] << '\n';
-    }
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N, M;
+	int S[100001];
+	cin >> N >> M;
+	for (int i = 0; i < N; i++) {
+		int A;
+		cin >> A;
+		S[i + 1] = S[i] + A;
+	}
+
+	for (int i = 0; i < M; i++) {
+		int s, e;
+		cin >> s >> e;
+		cout << S[e] - S[s - 1] << '\n';
+	}
 }
