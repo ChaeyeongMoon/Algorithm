2a12caffa5271c77ac31663e462a946b7aa34a3e
ChaeyeongMoon <121281644+ChaeyeongMoon@users.noreply.github.com>
2023-04-04 20:37:30 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 128 ms, Memory: 10108 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -1,5 +1,4 @@
 #include<iostream>
-#include<vector>
 using namespace std;
 
 int main() {
@@ -9,24 +8,23 @@ int main() {
 
 	int N, M;
 	cin >> N >> M;
-	
-	vector<vector<int>>A(N + 1, vector<int>(N + 1, 0));
-	vector<vector<int>>D(N + 1, vector<int>(N + 1, 0));
+
+	int A[1025][1025] = { 0 };
+	int D[1025][1025] = { 0 };
 
 	for (int i = 1; i <= N; i++) {
 		for (int j = 1; j <= N; j++) {
 			cin >> A[i][j];
 
-			//구간 합 구하기
-			D[i][j] = D[i][j - 1] + D[i - 1][j] - D[i - 1][j - 1] + A[i][j];
+			D[i][j] = D[i - 1][j] + D[i][j - 1] - D[i - 1][j - 1] + A[i][j];
 		}
 	}
 
 	for (int i = 0; i < M; i++) {
 		int x1, y1, x2, y2;
 		cin >> x1 >> y1 >> x2 >> y2;
-		
-		int result = D[x2][y2] - D[x1 - 1][y2] - D[x2][y1 - 1] + D[x1 - 1][y1 - 1];
-		cout << result << '\n';
+
+		int res = D[x2][y2] - D[x1 - 1][y2] - D[x2][y1 - 1] + D[x1 - 1][y1 - 1];
+		cout << res << '\n';
 	}
 }
