9db8bfe78c0be2f31c9230eec572e361805f44cc
ChaeyeongMoon <121281644+ChaeyeongMoon@users.noreply.github.com>
2023-05-02 21:50:30 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 40 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
@@ -1,25 +1,26 @@
 #include<iostream>
 using namespace std;
 
-
-
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
 	int N, M;
-	int S[100001];
 	cin >> N >> M;
-	for (int i = 0; i < N; i++) {
+
+	int S[100001] = { 0 };
+
+	for (int i = 1; i <=N; i++) {
 		int A;
 		cin >> A;
-		S[i + 1] = S[i] + A;
+		S[i] =S[i-1] + A;
 	}
 
 	for (int i = 0; i < M; i++) {
-		int s, e;
-		cin >> s >> e;
-		cout << S[e] - S[s - 1] << '\n';
+		int a, b;
+		cin >> a >> b;
+
+		cout << S[b] - S[a - 1]<<"\n";
 	}
 }
