c0b70d1e03fb9ab1bf5c0af48ecdb2a8910232d9
ChaeyeongMoon <121281644+ChaeyeongMoon@users.noreply.github.com>
2023-05-11 20:05:27 +0900

    [Silver II] Title: DFS와 BFS, Time: 0 ms, Memory: 2300 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -1,12 +1,11 @@
 #include<iostream>
-#include<queue>
 #include<algorithm>
 #include<vector>
+#include<queue>
 using namespace std;
 
-static vector<vector<int>>edge;
+static vector<vector<int>>A;
 static vector<bool>visited;
-
 void DFS(int node);
 void BFS(int node);
 
@@ -18,108 +17,52 @@ int main() {
 	int N, M, V;
 	cin >> N >> M >> V;
 
-	edge.resize(N + 1);
-	visited = vector < bool>(N + 1, false);
-	
+	A.resize(N + 1);
+	visited = vector<bool>(N + 1, false);
+
 	for (int i = 0; i < M; i++) {
 		int s, e;
 		cin >> s >> e;
 
-		edge[s].push_back(e);
-		edge[e].push_back(s);
+		A[s].push_back(e);
+		A[e].push_back(s);
 	}
 
 	for (int i = 1; i <= N; i++) {
-		sort(edge[i].begin(), edge[i].end());
+		sort(A[i].begin(), A[i].end());
 	}
 
 	DFS(V);
-	visited = vector<bool>(N + 1, false);
 	cout << "\n";
+	fill(visited.begin(), visited.end(), false);
 	BFS(V);
+	cout << "\n";
 }
 
 void DFS(int node) {
-	cout << node << " ";
-	visited[node] = 1;
-	for (int i : edge[node]) {
+	cout << node<<" ";
+	visited[node] = true;
+	for (int i : A[node]) {
 		if (visited[i] == false) {
 			DFS(i);
-			//visited[i] = true;
 		}
 	}
 }
 
 void BFS(int node) {
-	queue<int>q;
-	q.push(node);
+	queue<int>myqueue;
+	myqueue.push(node);
 	visited[node] = true;
-	while (q.size()) {
-		cout << q.front() << " ";
-		for (int i : edge[q.front()]) {
+	
+	while (!myqueue.empty()) {
+		int now_node = myqueue.front();
+		myqueue.pop();
+		cout << now_node << " ";
+		for (int i : A[now_node]) {
 			if (visited[i] == false) {
-				q.push(i);
 				visited[i] = true;
+				myqueue.push(i);
 			}
 		}
-		q.pop();
 	}
 }
-
-
-//#include<iostream>
-//#include<queue>
-//
-//using namespace std;
-//
-//int visiteddfs[1001];
-//int visitedbfs[1001];
-//
-//vector<int>edge[1001];
-//
-//void dfs(int node) {
-//	cout << node << " ";
-//	visiteddfs[node] = 1;
-//	for (auto x : edge[node]) {
-//		if (visiteddfs[x] != 1) {//방문이 안 되어있으면
-//			dfs(x);
-//		}
-//	}
-//	
-//}
-//
-//void bfs(int node) {
-//	queue<int>q;
-//	q.push(node);
-//	visitedbfs[node] = 1;//첫 노드를 방문했다고 표시
-//	while (q.size()) {//큐에 뭔가 남아있다몀ㄴ
-//		cout << q.front() << " ";
-//		for (auto x : edge[q.front()]) {
-//			if (visitedbfs[x] != 1) {
-//				visitedbfs[x] = 1;
-//				q.push(x);
-//			}
-//		}
-//		q.pop();//방금 살펴본 정점을 큐에서 빼준다
-//	}
-//}
-//
-//int main() {
-//	int n, m;
-//	cin >> n >> m;
-//
-//	int v;
-//	cin >> v;
-//
-//	for (int i = 0; i < m; i++) {
-//		int a, b;
-//		cin >> a >> b;
-//
-//		edge[a].push_back(b);
-//		edge[b].push_back(a);
-//
-//	}
-//	dfs(v);
-//	cout << "\n";
-//	bfs(v);
-//}
